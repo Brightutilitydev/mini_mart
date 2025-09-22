d3301908fa19f0fe2647b837557b9f03818fb178
Zealina Hudson <zealinahudson@gmail.com>
2025-09-22 21:00:18 +0100

    save images

--- api/v1/views/products.py
+++ api/v1/views/products.py
@@ -4,6 +4,14 @@
 from api.v1.views import app_views
 from flask import jsonify, request
 from repositories.product_repo import ProductRepo
+from werkzeug.utils import secure_filename
+import os
+
+
+allowed_extensions = [
+    ".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".tif",
+    ".webp", ".heic", ".heif", ".svg", ".ico", ".jfif", ".avif"
+]
 
 
 @app_views.route('/products', methods=['GET'])
@@ -23,10 +31,26 @@ def get_product(product_id):
     return jsonify({"error": "product not found"}), 404
 
 
+@app.route("/images/<filename>")
+def get_image(filename):
+    return send_from_directory(UPLOAD_FOLDER, filename)
+
+
 @app_views.route('/products/create', methods=['POST', 'PUT'])
 def create_product():
     """Create a new product"""
+    images = request.files.getlist('images')
+    if images:
+        for image in images:
+            ext = os.path.splittext(image.filename)[-1]
+            if ext.lower() not in allowed_extensions:
+                continue
+            filename = secure_filename(image.filename)
+            saved_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            image.save(saved_path)
     data = request.get_json()
+    data["image_url"] = url_for('get_image', filename=filename, _external=True)
+
     try:
         new = ProductRepo.new(**data)
     except ValueError as e:
