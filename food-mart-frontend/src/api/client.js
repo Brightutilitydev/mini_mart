78d3856187ac3d4a544d02badb60de71f73111b2
Brightutilitydev <masterbright02@gmail.com>
2026-06-22 09:46:37 +0100

    Update API client base URL for axios

--- food-mart-frontend/src/api/client.js
+++ food-mart-frontend/src/api/client.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-const apiClient = axios.create({ 
-  baseURL: 'https://mini-mart-aw0d.onrender.com/api/v1', // Make sure this matches your Flask route prefix!
-  withCredentials: true 
-});
+const apiClient = axios.create({ baseURL: 'https://mini-mart-aw0d.onrender.com', withCredentials: true });
+
 
 export default apiClient;
